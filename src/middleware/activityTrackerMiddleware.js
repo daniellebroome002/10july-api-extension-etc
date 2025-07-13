6f10fea22892b5148d8c81103bdc38eb5b47a334
daniellebroome002 <daniellebroome002@gmail.com>
2025-07-13 23:37:21 +0600

    Delete src directory

--- src/middleware/activityTrackerMiddleware.js
+++
@@ -1,9 +0,0 @@
-import { trackActivity } from '../services/activityTracker.js';
-
-export function activityTrackerMiddleware(req, res, next) {
-  // Track this request for real-time monitoring
-  trackActivity(req, res);
-  
-  // Continue to the next middleware
-  next();
-} 
